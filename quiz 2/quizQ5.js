98ae148ccd2f8985628a3e97bf625011772a44ae
Krista Brehm <kristabrehm1@gmail.com>
2019-01-11 18:00:15 -0500

    got rid of the split, added in more if statements to take into account the upper and lowercase codes and skip around the symbols between it

--- quiz 2/quizQ5.js
+++ quiz 2/quizQ5.js
@@ -1,2 +1,5 @@
 // 5. Write a program to find the largest of three given integers.
 var nums = [1, 2, 3];
+
+//apply allows you to use the function
+Math.max.apply(null, nums)
--- quiz 2/quizQ6.js
+++ quiz 2/quizQ6.js
@@ -5,30 +5,27 @@
 //then string join once the loop has gone through all the letters
 
 function nextLetter(string) {
-    //splits it at each letter, you dont want a space between the single quotes because then it will look for spaces in the string
-    string = string.split('');
+    let newString = '';
     //create a loop to cycle through each item(letter)
     for (var i = 0; i < string.length; i++) {
-        //the alphabets code is between 96 and 123 so you want to make sure that is is between those so this checks for that
+        //the uppercase alphabets code is between 90 and 65and lowercase is 97 to 122 so you want to make sure that is is between those so this checks for that
         //but the unicode after z is a symbol so this will take the code for z and redirect it to a
-        //for some reason its seying string.charCodeAt is not a function, not sure why
-        if (122 == string.charCodeAt(i)) {
-            result += "a";
-            //just needed to change it from 123 to 121 to take z out of it
-        } else if (96 < string.charCodeAt(i) && string.charCodeAt(1) < 121) {
+        if (122 === string.charCodeAt(i)) {
+            newString += string.fromCharCode(97);
+        } else if (90 === string.charCodeAt(i)) {
+            newString += string.fromCharCode(65);
+        } else if (65 < string.charCodeAt(i) && string.charCodeAt(i) < 90) {
             //so I dont want to use replace since it will replace each instance of a letter everytime you come to it so say you used abc
             //it would replace a with b and the b with c but it will replace the letter before it as well so it goes abc to bbc to ccc
             //to ddd which is not what we want here dont need the [i] part for this problem, so instead we want to create a new string
-            //String.fromCharAt returns a string based on the letter code
+            //String.fromCharCode returns a string based on the letter code
             //will need to add 1 onto the charCodeAt element to get the next letter
-            newString += String.fromCharAt(string.charCodeAt(i) + 1);
-            //this adds the changed letter to the string and moves it on to the next letter
-        } else {
-            result += string.charAt(i);
+            newString += String.fromCharCode(string.charCodeAt(i) + 1);
+        } else if (97 < string.charCodeAt(i) && string.charCodeAt(i) < 121) {
+            newString += String.fromCharCode(string.charCodeAt(i) + 1);
         }
-        //this joins all the strings back together into one string
-        return string.join(' ');
     }
+    return newString;
 }
-//obviously this runs the function
-nextLetter("Hello");
+//obviously this logs the result
+console.log(nextLetter("Hello"));
