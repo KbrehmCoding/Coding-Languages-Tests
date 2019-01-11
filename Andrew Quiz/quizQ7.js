2207b4e20f2508d7fd30a5ab9a8721da6b1d211b
Krista Brehm <kristabrehm1@gmail.com>
2019-01-11 14:47:01 -0500

    completed question 7

--- Andrew Quiz/quizQ7.js
+++ Andrew Quiz/quizQ7.js
@@ -1,8 +1,15 @@
 // 7. Write a program to capitalize the first letter of each word of a given string.
-var string = "i want pizza please";
 
 function CapitilizeIt(string) {
-    string.split(" ").toUpperCase
+    //this converts all letters to lower case, then splits it at the spaces, you need to add the space or else it will return an array item for each letter instead of each word
+    string = string.toLowerCase().split(' ');
+    //string[1] is the single word from the sentence, then you create a loop to cycle through each item(word), the 0 in charAt
+    //is the first letter of the word, it then uppercases the letter at charAt[0], and the slice takes out the word from index 1 to the end of the string
+    for (var i = 0; i < string.length; i++) {
+        string[i] = string[i].charAt(0).toUpperCase() + string[i].slice(1);
+    }
+    //this joins all the strings back together into one string/sentence
+    return string.join(' ');
 }
-
-CapitilizeIt(string);
+//obviously this runs the function
+CapitilizeIt("I want pizza please");
