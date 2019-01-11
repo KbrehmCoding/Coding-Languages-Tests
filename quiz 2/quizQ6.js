ab4a856e9322e3c3c9d478815a129d41ab78e6a5
Krista Brehm <kristabrehm1@gmail.com>
2019-01-11 15:21:57 -0500

    started making the loop to cycle through the letters using the charCodeAt to check the code for them, along with other parts of the loop/function

--- quiz 2/quizQ6.js
+++ quiz 2/quizQ6.js
@@ -3,3 +3,20 @@
 //probably need to create a loop that splits all the letters into seperate array items
 //maybe need to use the code for the alphapet to change the letters, like maybe i++ to the code that then returns the letter
 //then string join once the loop has gone through all the letters
+
+function nextLetter(string) {
+    //splits it at each letter, you dont want a space between the single quotes because then it will look for spaces in the string
+    string = string.split('');
+    //create a loop to cycle through each item(letter)
+    for (var i = 0; i < string.length; i++) {
+        //the alphabets code is between 96 and 123 so you want to make sure that is is between those so this checks for that
+        if (96< string.charCodeAt(i)&& string.charCodeAt(1)< 123) {
+    //the 0 in charAt is each letter since we split it up to single letters
+            string[i] = string[i].charAt(0)
+        }
+    }
+    //this joins all the strings back together into one string
+    return string.join(' ');
+}
+//obviously this runs the function
+nextLetter("Hello");
