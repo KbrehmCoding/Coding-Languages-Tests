5854e57155d27fdfb8f4e55d31633c5b736ed586
Krista Brehm <kristabrehm1@gmail.com>
2019-01-11 16:27:03 -0500

    commentsing about a problem I am having

--- quiz 2/quizQ6.js
+++ quiz 2/quizQ6.js
@@ -10,18 +10,25 @@ function nextLetter(string) {
     //create a loop to cycle through each item(letter)
     for (var i = 0; i < string.length; i++) {
         //the alphabets code is between 96 and 123 so you want to make sure that is is between those so this checks for that
-        if (96< string.charCodeAt(i)&& string.charCodeAt(1)< 123) {
-    //so I dont want to use replace since it will replace each instance of a letter everytime you come to it so say you used abc
-    //it would replace a with b and the b with c but it will replace the letter before it as well so it goes abc to bbc to ccc
-    //to ddd which is not what we want here dont need the [i] part for this problem, so instead we want to create a new string
-    //String.fromCharAt returns a string based on the letter code
-    //will need to add 1 onto the charCodeAt element to get the next letter
-    //but it probably wont handle z correctly sice the next unicode after it is not a letter but a symbol
+        //but the unicode after z is a symbol so this will take the code for z and redirect it to a
+        //for some reason its seying string.charCodeAt is not a function, not sure why
+        if (122 == string.charCodeAt(i)) {
+            result += "a";
+            //just needed to change it from 123 to 121 to take z out of it
+        } else if (96 < string.charCodeAt(i) && string.charCodeAt(1) < 121) {
+            //so I dont want to use replace since it will replace each instance of a letter everytime you come to it so say you used abc
+            //it would replace a with b and the b with c but it will replace the letter before it as well so it goes abc to bbc to ccc
+            //to ddd which is not what we want here dont need the [i] part for this problem, so instead we want to create a new string
+            //String.fromCharAt returns a string based on the letter code
+            //will need to add 1 onto the charCodeAt element to get the next letter
             newString += String.fromCharAt(string.charCodeAt(i) + 1);
+            //this adds the changed letter to the string and moves it on to the next letter
+        } else {
+            result += string.charAt(i);
         }
+        //this joins all the strings back together into one string
+        return string.join(' ');
     }
-    //this joins all the strings back together into one string
-    return string.join(' ');
 }
 //obviously this runs the function
 nextLetter("Hello");
