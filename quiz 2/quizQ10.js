d911f62d6dec4e0373c6347cc141ffbafdf7e4ed
Krista Brehm <kristabrehm1@gmail.com>
2019-01-13 17:51:37 -0500

    started on question 10 by defining the string variable to pass into the function, defined what letters are vowels by putting them in an array, and set up the console.log that will display the answer once I have written the function

---
+++ quiz 2/quizQ10.js
@@ -0,0 +1,10 @@
+// Write a program to count the number of vowels in a given string.
+
+// Will need to tell the computer which letters are vowels
+// How to count the number of ocurences instead of just returning the vowels themselves
+
+var string = "Krista";
+
+var vowels = ["a", "e", "i", "o", "u", "y"];
+
+console.log("There are" + vowelNumbers + "in" + string);
