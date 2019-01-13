ca3358caf5da63372fd56b451d309c984f60c117
Krista Brehm <kristabrehm1@gmail.com>
2019-01-13 17:39:04 -0500

    uesed console.log to show the answer given by Math.max, passed the array into Math.max, and used the ... to get it to read the array correctly

--- quiz 2/quizQ5.js
+++ quiz 2/quizQ5.js
@@ -1,5 +1,5 @@
 // 5. Write a program to find the largest of three given integers.
-var nums = [1, 2, 3];
+var numbers = [1, 2, 3];
 
-//apply allows you to use the function
-Math.max.apply(null, nums)
+    console.log(Math.max(...numbers));
+
