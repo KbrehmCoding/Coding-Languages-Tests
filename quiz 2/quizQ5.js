52603d37b2f16ccb9c8c6155b659ee74a3a2cfd2
Krista Brehm <kristabrehm1@gmail.com>
2019-01-13 22:40:06 -0500

    redid question 5 so that it finds the largest number without math.max

--- quiz 2/quizQ5.js
+++ quiz 2/quizQ5.js
@@ -1,5 +1,14 @@
 // 5. Write a program to find the largest of three given integers.
 var numbers = [1, 2, 3];
 
-    console.log(Math.max(...numbers));
+function compareNumbers(array) {
+    var largestNumber = "";
+    array.forEach(function(numbers){
+        if(numbers > largestNumber) {
+            largestNumber = numbers;
+            }
+        });
+    return largestNumber;
+}
 
+    console.log("The largest number is " + compareNumbers(numbers));
