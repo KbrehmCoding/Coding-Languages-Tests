9a6bdfe1f7f14ad08306a93b0afbc464ebca55df
Krista Brehm <kristabrehm1@gmail.com>
2019-01-13 20:06:17 -0500

    fixed problem 9 so that it alphabetizes the letters in a string rather than in an array

--- quiz 2/quizQ9.js
+++ quiz 2/quizQ9.js
@@ -1,7 +1,17 @@
 // Write a program to convert the letters of a given string in alphabetical order.
 
-var letters = ["d", "r", "e", "o", "l", "a", "z"];
+var string = "banana";
 
-letters.sort();
-
-console.log(letters);
+function sortLetters(string) {
+    // Splits the string into seperate letters
+    string = string.split('');
+    //string[1] is the single word from the sentence, then you create a loop to cycle through each item(letter)
+    //is the first letter of the word, it then uppercases the letter at charAt[0], and the slice takes out the word from index 1 to the end of the string
+    for (var i = 0; i < string.length; i++) {
+        string.sort();
+    }
+    //this joins all the strings back together into one string/sentence
+    return string.join('');
+}
+//obviously this runs the function
+console.log(sortLetters(string));
