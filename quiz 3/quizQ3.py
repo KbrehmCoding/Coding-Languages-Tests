4e36cc9bd2cb75afa2b6977da43202c399225a02
Krista Brehm <kristabrehm1@gmail.com>
2019-02-27 22:45:19 -0500

    answer to #3, found out I was writing in the wrong version of python

--- quiz 3/quizQ3.py
+++ quiz 3/quizQ3.py
@@ -1,27 +1,23 @@
 #  3. Write a program to calculate multiplication and division of two numbers (input from user).
 
-# TODO look up pyhon code to take user input in  the command line and use it in math functions
-num1 = int(input("Please input first number: "))
-num2 = int(input("Please input second number: "))
+num1 = int(raw_input("Please input first number: "))
+num2 = int(raw_input("Please input second number: "))
 
 def multiply():
-    num1 = (num1).val()
-    num2 = (num2).val()
-    multiply = num1 * num2
-    sumMulti = num1 + " Multiplied by " + num2 + " is " + multiply
-    print(sumMulti)
+    print "{} multiplied by {} is {}".format(num1, num2, num1 * num2)
 
 def divide():
-    num1 = (num1).val()
-    num2 = (num2).val()
-    divide = num1 / num2
-    sumDivi = num1 + " Divided by " + num2 + " is " + divide
-    print(sumDivi)
+    print "{} divided by {} is {}".format(num1, num2, num1 / num2)
+
+operator = raw_input("Would you like to multiply these numbers, or divide them? : ")
+
+while operator != "multiply" and operator != "divide":
+    operator = raw_input("Would you like to multiply these numbers, or divide them? : ")
+
+if operator == "multiply":
+    multiply()
+elif operator == "divide":
+    divide()
 
 
-operator = input("Would you like to multiply these numbers, or divide them? : ")
-while operator != "multiply" or operator != "divide":
-    if operator == "multiply":
-        multiply()
-    elif operator == "divide":
-        devide()
+
--- quiz 3/quizQ4.py
+++ quiz 3/quizQ4.py
@@ -1,9 +1,7 @@
-num1 = int(input("Enter temperature in Fahrenheit: "))
+numGiven = int(input("Enter temperature in Fahrenheit: "))
 def convert(input):
-    numGiven = num1
     step1 = numGiven - 32
     step2 = step1 * 5
     step3 = step2 / 9
     print(step3 + " Degrees Celsius")
 
-convert()
