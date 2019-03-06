305a833457078147fe5027a1525d7c349b581252
Krista Brehm <kristabrehm1@gmail.com>
2019-03-06 18:17:43 -0500

    finished question 5 in python

--- quiz 3/quizQ5.py
+++ quiz 3/quizQ5.py
@@ -1,15 +1,13 @@
 # // 5. Write a program to find the largest of three given integers.
-
-numbers = [1, 2, 3]
+numbers = [13, 2, 10]
 
 def compareNumbers():
-    largestNumber = ""
-    array.forEach(def(numbers)
-        if(numbers > largestNumber)
-            largestNumber = numbers
+    if numbers[0] > numbers[1] and numbers[0] > numbers [2]:
+        print "The largest number is {}".format(numbers[0])
+    elif numbers[1] > numbers[0] and numbers[1] > numbers [2]:
+        print "The largest number is {}".format(numbers[1])
+    else:
+        print "The largest number is {}".format(numbers[2])
 
-        )
-    return largestNumber
 
-
-    print("The largest number is " + compareNumbers(numbers))
+compareNumbers()
