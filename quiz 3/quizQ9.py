b7b6162932a2e877827c984970ecc1facc841cc2
Krista Brehm <kristabrehm1@gmail.com>
2019-03-10 19:07:14 -0400

    finished question 9

--- quiz 3/quizQ9.py
+++ quiz 3/quizQ9.py
@@ -1,2 +1,6 @@
-# // Write a program to convert the letters of a given string in alphabetical order.
+# Write a program to convert the letters of a given string in alphabetical order.
+# in python you only need to join it and sort it and label the new string a new name so as not o just print the original string
+string = "krista"
+sortedString = ''.join(sorted(string))
+print(sortedString)
 
