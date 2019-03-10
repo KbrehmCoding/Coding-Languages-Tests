e34bfb7e1170cbdf38ec0d91e411267304743c93
Krista Brehm <kristabrehm1@gmail.com>
2019-03-10 17:58:29 -0400

    answer to python question 8

--- quiz 3/quizQ8.py
+++ quiz 3/quizQ8.py
@@ -2,3 +2,11 @@
 
 
 # devide the numberGiven by 60 to get the hours then label the remainder as minutes
+numGiven =  int(raw_input("Enter a number to find out how many hours and minutes it is: "))
+hours = numGiven / 60
+minutes = numGiven % 60
+
+def howLong():
+    print "{} is {} hours and {} minutes".format(numGiven, hours, minutes)
+
+howLong()
