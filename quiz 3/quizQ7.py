61aed2bfaaabd865c523b623964235eecd25e6cb
Krista Brehm <kristabrehm1@gmail.com>
2019-03-10 17:28:53 -0400

    sudo coded the problem then coded each step, then merged them all together on one line

--- quiz 3/quizQ7.py
+++ quiz 3/quizQ7.py
@@ -1,8 +1,15 @@
 # Write a program to capitalize the first letter of each word of a given string.
 # to split stings in python (a.split(" "))
+# how to affect only thee first letter of each item in an array "closets I can find so far is string[0][0]"
 # to caitalize letters in python (a.upper())
 # Connect the string back together string = "-".join(string)
 # Return the new string
 
+string = "hello world"
 
 def capitalizeIt() :
+    print(' '.join(word[0].upper() + word[1:] for word in string.split()))
+
+
+capitalizeIt()
+
