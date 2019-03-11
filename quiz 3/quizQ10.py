d5124dde73923cc999a9d09a0ed656efad2eb12e
Krista Brehm <kristabrehm1@gmail.com>
2019-03-11 16:14:56 -0400

    answered question 10

--- quiz 3/quizQ10.py
+++ quiz 3/quizQ10.py
@@ -1,5 +1,16 @@
-#  Write a program to count the number of vowels in a given string.
+# Write a program to count the number of vowels in a given string.
 
-# // Will need to tell the computer which letters are vowels
-# // How to count the number of ocurences instead of just returning the vowels themselves
-# //may need to break the string down into an array of letters that are then compared to the vowels array
+# Will need to tell the computer which letters are vowels
+# Increment the number of vowels with each one it finds
+
+string = "krista"
+
+
+def howMany():
+    numVowels = 0
+    for char in string:
+        if char in "aeiouAEIOU":
+            numVowels += 1
+    print numVowels
+
+howMany()
--- quiz 3/quizQ6.py
+++ quiz 3/quizQ6.py
@@ -1,21 +1,5 @@
 # Write a program to replace every character in a given string with the character following it in the alphabet.
 
+string = "hello"
 def nextLetter(string):
-    newString = ''
-    i = 0
-    # Todo research python for loops
-    for x in string:
-        if 122 == ord(i):
-            newString += chr(97)
-        elif 90 == ord(i):
-            newString += chr(65)
-        elif 65 < ord(i) and string.ord(i) < 90 :
-            newString += chr(ord(i) + 1)
-        elif 97 < ord(i) and ord(i) < 121 :
-            newString += chr(ord(i) + 1)
 
-    return newString
-
-print (nextLetter("Hello"))
-
-nextLetter()
