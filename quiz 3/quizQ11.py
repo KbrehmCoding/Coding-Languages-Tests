ce63791bdc8a3446b67c7ab037d90668a8016634
Krista Brehm <kristabrehm1@gmail.com>
2019-03-11 16:18:47 -0400

    sudo coded out what I need to do to answer question 11

--- quiz 3/quizQ11.py
+++ quiz 3/quizQ11.py
@@ -1,2 +1,7 @@
-# // Write a program to find the longest string from a given array.
+# Write a program to find the longest string from a given array.
 
+# Will need to create a loop
+# Use len to count the length of each item in array
+# When a item is found to be longer than the longest word,
+# it will become the new value for longest word
+# Final step is to print the longest word
