ccaebed406b155b43327c52057f26885c34eff4b
Krista Brehm <kristabrehm1@gmail.com>
2019-03-31 19:05:54 -0400

    completed the first question in swift

--- quiz3/quizQ1.swift
+++ quiz3/quizQ1.swift
@@ -2,13 +2,12 @@
 var a = 5
 var b = 6
 var c = 7
-var s = Double (a+b+c) / 2
+var s: Int = (a+b+c) / 2
 
-
-var p1 = ( s - a )
-var p2 = ( s - b )
-var p3 = ( s - c )
-var p4 = Double (s*(p1*p2*p3)
-var area = Double (p4*0.5)
+var p1: Int = ( s - a )
+var p2: Int = ( s - b )
+var p3: Int =  (s - c )
+var p4: Double = Double(s*(p1*p2*p3))
+var area: Double = Double(p4).squareRoot()
 
 print(area)
