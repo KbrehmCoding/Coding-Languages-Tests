760767e84a88e2a2a842ddc293c9c5b8ed1ff0cc
Krista Brehm <kristabrehm1@gmail.com>
2019-03-31 16:17:38 -0400

    starting on swift questions

---
+++ quiz3/quizQ1.swift
@@ -0,0 +1,14 @@
+// 1. Write a program to find the area of a triangle where lengths of the three of its sides are 5, 6, 7.
+var a = 5
+var b = 6
+var c = 7
+var s = Double (a+b+c) / 2
+
+
+var p1 = ( s - a )
+var p2 = ( s - b )
+var p3 = ( s - c )
+var p4 = Double (s*(p1*p2*p3)
+var area = Double (p4*0.5)
+
+print(area)
--- quiz3/quizQ12.py
+++ quiz3/quizQ12.py
@@ -2,5 +2,20 @@
 
 # // Possibly need to split the string into an array of letters then deal with them each individually
 # // Need to use toUpperCase and toLowerCase within if statements to check the current case of the letter so it can be swtiched
+string = "North Carolina"
+
+def case_switch():
+    list(string)
+    for x in string:
+        if string.isupper():
+            string.lower()
+        elif string.islower():
+            string.upper()
+    newString = string.join("")
+    print(newString)
+
+case_switch()
 
 
+# shortcut
+print (string.swapcase())
