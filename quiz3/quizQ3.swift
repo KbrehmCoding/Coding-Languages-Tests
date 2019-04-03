532f5c94d81070bd04a625fded5b126be1cceda0
Krista Brehm <kristabrehm1@gmail.com>
2019-04-02 23:51:47 -0400

    spent the day at AWS nd then learned more about swift so no new code but some comments

---
+++ quiz3/quizQ3.swift
@@ -0,0 +1,6 @@
+// 3. Write a program to calculate multiplication and division of two numbers (input from user).
+
+
+// you need to use : when asigning the type such as int and double, or string.
+//not sure why you need it when the numbers are represented by words but I will look into it
+//today I spent my time learning about swift so I did not get to code today.
