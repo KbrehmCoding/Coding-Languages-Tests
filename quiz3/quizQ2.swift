56995268bcf2aea0b630a764c7e658f27137a83c
Krista Brehm <kristabrehm1@gmail.com>
2019-04-03 22:20:58 -0400

    solved question 3 in swift

--- quiz3/quizQ2.swift
+++ quiz3/quizQ2.swift
@@ -3,12 +3,12 @@
 // Will need to create a for loop to iterate through the string
 // May need to seperate the letters of the string into individual characters then move,
 // the first letter and join them together each time the for loop iterates
-
+// Possibly need to use end index to grab the last letter and maybe prepend to attatch it to the begining?
+//still trying to find a way to do this without using substring
 func rotateThisString() {
     var word = "Chocolate"
     for x in word {
-        word.sufix(1)
-        word.prepend(sufix(1))
+
     }
 }
 
--- quiz3/quizQ3.swift
+++ quiz3/quizQ3.swift
@@ -2,5 +2,23 @@
 
 
 // you need to use : when asigning the type such as int and double, or string.
-//not sure why you need it when the numbers are represented by words but I will look into it
-//today I spent my time learning about swift so I did not get to code today.
+// learned that doubles are used for large decimals
+// The swift version of input in readLine() which you set as the value of a var
+// Will need to make a if then statement to handle the operator choice made by the user
+// Then print the results
+
+
+print("Would you like to multiply or divide (m/d)?")
+var userOperator = readLine() ?? "m"
+print("Please enter the first number")
+var num1 = Int(readLine()!) ?? 0
+print("Please enter the second number")
+var num2 = Int(readLine()!) ?? 0
+
+if userOperator == "m" {
+    print("\(num1) multiplied by \(num2) equals \(num1 * num2)")
+} else if userOperator == "d" {
+    print("\(num1) divided by \(num2) equals \(num1 / num2)")
+} else {
+    print("error")
+}
