f8f69e6584acd399050621c829d85307bd92bf72
Krista Brehm <kristabrehm1@gmail.com>
2019-04-06 20:23:59 -0400

    still working on the string manipulation

--- quiz3/php/quizQ10.php
+++ quiz3/php/quizQ10.php
@@ -1,5 +1,9 @@
 <!-- 10. Write a program to count the number of vowels in a given string. -->
 
 <?php
+    $string = "banana";
+    $stringParts = str_split($string);
+    function countVowels(){
 
+    }
 ?>
