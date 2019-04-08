528ab939fab6d8b630b9e024eb5e7921d633aacc
Krista Brehm <kristabrehm1@gmail.com>
2019-04-07 21:51:25 -0400

    answered question 10 in php

--- quiz3/php/quizQ10.php
+++ quiz3/php/quizQ10.php
@@ -2,17 +2,20 @@
 <?php
 // 10. Write a program to count the number of vowels in a given string.
 
-    $string = "banana";
+function countVowels($string) {
     $stringParts = str_split($string);
     $numVowels = 0;
-
-    function countVowels(){
-        foreach($stringParts as $i) {
-            if ($i == [ "a" , "A", "e" , "E" , "i" , "I" , "o" , "O" , "u" , "U" ]) {
-                $numVowels + 1;
-            }
+    $vowels = array("a", "A", "e", "E", "i", "I", "o", "O", "u", "U");
+    foreach ($stringParts as $i) {
+        if (in_array($i, $vowels)) {
+            $numVowels++;
         }
-        return $numVowles;
     }
-    echo "There are $numVowles vowels in $string \r\n";
+    return $numVowels;
+}
+
+$string = "banana";
+$numVowels = countVowels($string);
+
+echo "There are $numVowels vowels in $string\r\n";
 ?>
