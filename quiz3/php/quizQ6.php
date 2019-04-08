464ad0829691c5f9368c62122fd6ffd0905a51a6
Krista Brehm <kristabrehm1@gmail.com>
2019-04-07 23:41:04 -0400

    worked a bit more on question 6 in php

--- quiz3/php/quizQ6.php
+++ quiz3/php/quizQ6.php
@@ -8,24 +8,24 @@
 // Join the array back into a string
 //return the new string
 
-function nextLetter($string){
+function nextLetter($string) {
     $stringParts = str_split($string);
     $newString = " ";
     foreach($stringParts as $letter) {
-        if(ord($string[$index]) == 122) {
+        if (ord($stringParts[$index]) == 122) {
             $newString = ord(97);
-        } else if (ord($string[$index]) == 90){
+        } else if (ord($stringParts[$index]) == 90) {
             $newString = ord(65);
-        } else if (ord($string[$index]) > 65 && ord($string[$index])< 90) {
+        } else if (ord($stringParts[$index]) > 65 && ord($stringParts[$index]) < 90) {
             $newString = ord($string[$index]+1);
-        } else if (ord($string[$index]) > 97 && ord($string[$index]) < 121) {
-            $newString = ord($string[$index]+1);
+        } else if (ord($stringParts[$index]) > 97 && ord($stringParts[$index]) < 121) {
+            $newString = ord($stringParts[$index]+1);
         }
     }
     return $newString;
 }
 $string = "apple";
 $newString = nextLetter($string);
-print($newString);
+print("$newString\r\n");
 
 ?>
