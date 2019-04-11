0fd33a7748e5838a0edf4a95bbfb25f58c7a39f2
Krista Brehm <kristabrehm1@gmail.com>
2019-04-11 01:17:26 -0400

    undid changes to style.css and some of the changes to html

--- quiz4/game2/script.js
+++ quiz4/game2/script.js
@@ -1,4 +1,8 @@
 var backgroundColor = color(119, 89, 189);
+var x = canvas.width/2;
+var y = canvas.height-30;
+var ballRadius = 10;
+
 
 var setup = () => {
     fullScreen();
@@ -7,8 +11,17 @@ var setup = () => {
 
 var draw = () => {
     background(backgroundColor);
-    rect(100, mouseY, 40, 80);
-    rect(maxWidth - 100 - 40, mouseY, 40, 80);
+    pongBall();
+    rect(100, mouseY, 30, 80);
+    rect(maxWidth - 100 - 30, mouseY, 30, 80);
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
+    x += dx;
+    y += dy;
 };
 
 var mouseClicked = () => {
@@ -16,12 +29,18 @@ var mouseClicked = () => {
 };
 
 var pongBall = () => {
-    circle();
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
 };
 
 
-/* Need a two reclangles that will follow the cursor
+/* Define the edges of the feild so circle bounces off sides (done)
+Need a two reclangles that will follow the cursor (done)
+Make a moving circle
 A circle that bounces off the rectangle when it makes contact
 Have the circle bounce off at an angle
 Scores on each side of the screen
-When the ball goes past one of the rectangles the other side gets a point */
+When the circle goes past one of the rectangles the other side gets a point */
