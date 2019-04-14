5ad150bd01a68827294ec5490f4450a96141e592
Krista Brehm <kristabrehm1@gmail.com>
2019-04-13 23:12:05 -0400

    organizedthe code into components

---
+++ quiz4/game2/script.backup.js
@@ -0,0 +1,57 @@
+var backgroundColor = color(119, 89, 189);
+
+var setup = () => {
+    fullScreen();
+    background(backgroundColor);
+    // x = canvas.width/2;
+    // y = canvas.height-30;
+    // ballRadius = 10;
+};
+
+var draw = () => {
+    background(backgroundColor);
+    // pongBall();
+    rect(100, mouseY, 30, 80);
+    rect(maxWidth - 100 - 30, mouseY, 30, 80);
+    // if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+    //     dx = -dx;
+    // }
+    // if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+    //     dy = -dy;
+    // }
+    // x += dx;
+    // y += dy;
+};
+
+var mouseClicked = () => {
+    backgroundColor = randomGoldenRatioColor();
+};
+
+// let pongBall = () => {
+//     ctx.beginPath();
+//     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+//     ctx.fillStyle = "#F8BD32";
+//     ctx.fill();
+//     ctx.closePath();
+// };
+
+// let lastTime = 0;
+
+// function ballMovement(timestamp) {
+//     let deltaTime = timestamp - lastTime;
+//     lastTime = timestamp;
+
+//     pongBall.update(deltaTime);
+//     pongBall.draw(ctx);
+
+//     requestAnimationFrame(ballMovement);
+// }
+
+
+/* Define the edges of the feild so circle bounces off sides (done)
+Need a two reclangles that will follow the cursor (done)
+Make a moving circle
+A circle that bounces off the rectangle when it makes contact
+Have the circle bounce off at an angle
+Scores on each side of the screen
+When the circle goes past one of the rectangles the other side gets a point */
--- quiz4/game2/script.js
+++ quiz4/game2/script.js
@@ -1,57 +1,96 @@
+const game = new Game();
 
-let setup = () => {
+var setup = () => {
     fullScreen();
-    background(backgroundColor);
-    backgroundColor = color(119, 89, 189);
-    x = canvas.width/2;
-    y = canvas.height-30;
-    ballRadius = 10;
+    game.setup();
 };
 
-let draw = () => {
-    background(backgroundColor);
-    pongBall();
-    rect(100, mouseY, 30, 80);
-    rect(maxWidth - 100 - 30, mouseY, 30, 80);
-    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
-        dx = -dx;
+var draw = () => {
+    game.draw();
+};
+
+var mouseClicked = () => {
+    if (!game.started) {
+        game.startGame();
     }
-    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
-        dy = -dy;
-    }
-    x += dx;
-    y += dy;
 };
 
-let mouseClicked = () => {
-    backgroundColor = randomGoldenRatioColor();
-};
+function Game() {
+    this.components = [];
+    this.menuComponents = [];
+    this.started = false;
 
-let pongBall = () => {
-    ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = "#F8BD32";
-    ctx.fill();
-    ctx.closePath();
-};
+    this.setup = function () {
+        this.components = [
+            new Background(color(119, 89, 189)),
+            new Ball(),
+            new Paddle(),
+            new Paddle(true),
+        ];
+        this.menuComponents = [
+            new Background(randomPastelColor()),
+        ];
+    };
 
-let lastTime = 0;
+    this.draw = function () {
+        if (this.started) {
+            this.components.forEach(component => {
+                component.step && component.step();
+                component.draw && component.draw();
+            });
+        } else {
+            this.menuComponents.forEach(component => {
+                component.step && component.step();
+                component.draw && component.draw();
+            });
+        }
+    };
 
-function ballMovement(timestamp) {
-    let deltaTime = timestamp - lastTime;
-    lastTime = timestamp;
-
-    pongBall.update(deltaTime);
-    pongBall.draw(ctx);
-
-    requestAnimationFrame(ballMovement);
+    this.startGame = function () {
+        this.started = true;
+    };
 }
 
+function Background(backgroundColor) {
+    this.backgroundColor = backgroundColor;
+
+    this.draw = function () {
+        background(this.backgroundColor);
+    };
+}
+
+function Paddle(isRightPaddle) {
+    this.width = 30;
+    this.height = 80;
+
+    this.draw = function () {
+        rect(isRightPaddle ? maxWidth - 100 - 30 : 100, mouseY, this.width, this.height);
+    };
+}
+
+function Ball() {
+    this.x = 130;
+    this.y = 300;
+    this.xSpeed = 3;
+    this.ySpeed = 3;
+    this.radius = 10;
+
+    this.step = function () {
+        this.x += this.xSpeed;
+        this.y += this.ySpeed;
+        return this;
+    };
+
+    this.draw = function () {
+        circle(this.x, this.y, this.radius);
+    };
+}
 
 /* Define the edges of the feild so circle bounces off sides (done)
 Need a two reclangles that will follow the cursor (done)
-Make a moving circle
+Make a moving circle (done)
 A circle that bounces off the rectangle when it makes contact
 Have the circle bounce off at an angle
 Scores on each side of the screen
-When the circle goes past one of the rectangles the other side gets a point */
+When the circle goes past one of the rectangles the other side gets a point
+I'm not sure where to put the max width and max height vars to keep the ball inside the game area*/
