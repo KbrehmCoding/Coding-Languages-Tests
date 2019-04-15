920f73f2c7a73f3424adbd85e22b964c19fa996b
Krista Brehm <kristabrehm1@gmail.com>
2019-04-15 19:20:34 -0400

    working on question 12 in swift

--- quiz3/swift/quizQ12.swift
+++ quiz3/swift/quizQ12.swift
@@ -1,1 +1,16 @@
 // 12. Write a program to change the capitalization of all letters in a given string.
+var givenString = "cATS aRE aweSOME"
+
+
+func caseChange(givenString: String) -> String {
+    for i in givenString {
+        if isUpperCase(givenString(i)) {
+            i = i.lowercased()
+        } else if isLowerCase(givenString(i)) {
+            i = i.uppercased()
+        }
+    }
+    return givenString
+}
+
+print(caseChange(givenString))
