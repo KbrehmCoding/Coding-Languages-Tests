0b2a3508697bf4442a44b9806123d67ef7bb58b9
Krista Brehm <kristabrehm1@gmail.com>
2019-04-17 23:47:48 -0400

    updating code

--- quiz3/swift/quizQ9.swift
+++ quiz3/swift/quizQ9.swift
@@ -1,1 +1,8 @@
 // 9. Write a program to convert the letters of a given string in alphabetical order.
+
+var givenString = "liposuction"
+var givenArray = split(givenString)
+
+for character in givenArray {
+    UInt8(character) + 1
+}
